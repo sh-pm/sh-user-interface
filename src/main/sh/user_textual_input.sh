52d4424fd853174f804a1e793abdfeacc6d2b787
sh-pm <shpmmail@gmail.com>
2021-06-19 13:13:30 -0300

    v0.2.0
    - New release version

--- src/main/sh/user_textual_input.sh
+++ src/main/sh/user_textual_input.sh
@@ -5,8 +5,7 @@ source ../../../bootstrap.sh
 include_lib sh-logger
 include_lib sh-commons
 
-ensure_user_confirm_before_proceed() {
-	 
+ensure_user_confirm_before_proceed() {	 
 	declare -A params=( \
 		[DESCRIBE_SITUATION_MSG]="$1" \
 		[YES_NO_QUESTION]="$2" \
@@ -18,16 +17,33 @@ ensure_user_confirm_before_proceed() {
 	    exit "$FALSE"
 	fi
 	 
+	if ! is_user_confirm_before_proceed "${params[DESCRIBE_SITUATION_MSG]}" "${params[YES_NO_QUESTION]}" ; then
+    	log_warn "Abort operation"
+        exit 1;	
+	fi
+}
+
+is_user_confirm_before_proceed() {	 
+	declare -A params=( \
+		[DESCRIBE_SITUATION_MSG]="$1" \
+		[YES_NO_QUESTION]="$2" \
+	);
+	
+	# Check number of params 
+	if ! is_number_params_correct 2 "$@"; then
+		print_usage_help "$(declare -p params)" "$@" -f
+	    exit "$FALSE"
+	fi	 
 
 	echo "${params[DESCRIBE_SITUATION_MSG]}"
 	read -r -p "${params[YES_NO_QUESTION]} " response
+	echo "SUA RESPOSTA FOI: |$response|"
 	case "$response" in
-	    [yY][eE][sS]|[yY]) 
-	        return;
+	    [y]|[Y]|[e]|[E]|[s]|[S]|[y]|[Y])	        
+	        return "$TRUE";
 	        ;;
 	    *)
-	    	log_warn "Abort operation"
-	        exit 1;
+	        return "$FALSE";
 	        ;;
 	esac
 }
